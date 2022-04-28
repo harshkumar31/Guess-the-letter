8736e11c59f424f1bd5aa23c1524977cccdb2591
harshkumar31 <hkumar_be19@thapar.edu>
2022-04-29 00:18:24 +0530

    Update game.js

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ var level = 1;
 var row = 0;
 var col = 0;
 var attempts = 0;
-const word = ["qwemt","yuiobcvbnm","tacroqlbtmpgiqe"];
+const word = ["qwemt","yuiobcvbnm","tacroylbtmpgiye"];
 const ques = [["First letter of the name of person who invented Facebook","First letter of source of oxygen","The Great _all of China",
             "First letter of animal which has a trunk","First letter of the name of holy book of muslims"],["First letter of a vehicle that runs on water",
             "First letter of the only country which has an ocean named after it","First letter of an object that can attract metal objects to it",
